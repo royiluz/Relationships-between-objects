54a07dfc2ff3066a8fada16f0a4767d855a39e1a
royiluz <royi.iluz@gmail.com>
2020-12-08 12:23:06 +0200

    Using ObjectDetection and organize code

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-from imageai.Prediction import ImagePrediction
-import numpy as np
-import h5py
+from imageai.Detection import ObjectDetection
+import tensorflow as tf
 import os
 
+tf.compat.v1.disable_eager_execution()
 
-def convert_file(input_dir, filename, output_dir):
-    filepath = input_dir + '/' + filename
-    fin = open(filepath, 'rb')
-    binary_data = fin.read()
-    new_filepath = output_dir + '/' + filename[:-4] + '.hdf5'
-    f = h5py.File(new_filepath)
-    dt = h5py.special_dtype(vlen=np.dtype('uint8'))
-    data_set = f.create_dataset('binary_data', (100, ), dtype=dt)
-    data_set[0] = np.fromstring(binary_data, dtype='uint8')
+if __name__ == '__main__':
 
+    tf.compat.v1.disable_eager_execution()
+    detector = ObjectDetection()
 
-execution_path = os.getcwd()
-prediction = ImagePrediction()
-prediction.setModelTypeAsResNet()
-prediction.setModelPath(execution_path + "\\resnet50_weights_tf_dim_ordering_tf_kernels.h5")
-prediction.loadModel()
+    model_path = "./models/yolo.h5"
+    input_path = "./input/pic_3.jpeg"
+    output_path = "./output/new_pic.jpg"
 
+    # detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(model_path)
+    detector.loadModel()
+    detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
 
-predictions, percentage_probabilities = prediction.predictImage(execution_path + "\\car.jpeg", result_count=5)
-for index in range(len(predictions)):
-    print(predictions[index], " : ", percentage_probabilities[index])
-
+    for eachItem in detection:
+        print(eachItem["name"], " : ", eachItem["percentage_probability"])
