a6992fdc0167acb9cd5ebd2d28bc915966cb8f1e
royiluz <royi.iluz@gmail.com>
2021-01-18 16:16:27 +0200

    Fix some warning lines

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from imageai.Detection import ObjectDetection
 import tensorflow as tf
-import os
 
-tf.compat.v1.disable_eager_execution()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     tf.compat.v1.disable_eager_execution()
     detector = ObjectDetection()
 
     model_path = "./models/yolo.h5"
-    input_path = "./input/pic_3.jpeg"
-    output_path = "./output/new_pic_2.jpg"
+    # input_path = "./input/pic_11.jpg"
+    input_path = "./input/pic_22.jpeg"
+    output_path = "./new_pic.jpg"
 
     # detector.setModelTypeAsTinyYOLOv3()
     detector.setModelTypeAsYOLOv3()
