8bd5a80626b29ae73fddf1268acf14f0844669a0
royiluz <royi.iluz@gmail.com>
2021-06-23 17:32:20 +0300

    Success detecting chair behind/in front to table Add loading animation while waiting for results

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
+import cv2
 from imageai.Detection import ObjectDetection
 import tensorflow as tf
 from flask import Flask, render_template, request
 from PIL import Image
 import base64
 import io
+import numpy as np
+from keras.models import load_model
 
 
 def getRelations(img_path):
-
-    model_path = "./models/yolo.h5"
-    output_path = "./new_pic.jpg"
-
     # Constants
     TABLE = "dining table"
     CHAIR = "chair"
     TABLE_CHAIR = [TABLE, CHAIR]
 
-    # Create detector with yolo
+    # Load Yolo
+    model_path = "./models/yolo.h5"
     detector = ObjectDetection()
     detector.setModelTypeAsYOLOv3()
     detector.setModelPath(model_path)
     detector.loadModel()
+
+    output_path = "./new_pic.jpg"
     detection = detector.detectObjectsFromImage(input_image=img_path, output_image_path=output_path)
     relations = []
     names = {}
@@ -35,8 +37,14 @@ def getRelations(img_path):
             obj_1 = detection[i]
             obj_2 = detection[j]
 
+            # Make sure chair is obj_1 and table is obj_2 (if exist!):
+            if obj_2["name"] == CHAIR and obj_1["name"] == TABLE:
+                temp = obj_1
+                obj_1 = obj_2
+                obj_2 = temp
+
             # Make sure that CHAIR/TABLE is obj_1:
-            if obj_2["name"] in TABLE_CHAIR:
+            elif obj_2["name"] in TABLE_CHAIR:
                 temp = obj_1
                 obj_1 = obj_2
                 obj_2 = temp
@@ -46,11 +54,11 @@ def getRelations(img_path):
             obj_1_width = obj_1_x2 - obj_1_x1
             obj_2_width = obj_2_x2 - obj_2_x1
 
-            # Check relation between Cup/Glass and Chair:
+            # Check relation between Objects and Chair:
             if (obj_1["name"] == CHAIR) and (obj_2["name"] not in TABLE_CHAIR):
                 chair_height = obj_1_y2 - obj_1_y1
                 chair_width = obj_1_width
-                # If cup between the chair x borders:
+                # If Object between the chair's x borders:
                 if obj_1_x1 - chair_width * 0.2 < obj_2_x1 and obj_2_x2 < obj_1_x2 + chair_width * 0.2:
                     if obj_1_y1 < obj_2_y1:
                         if obj_1_y1 + chair_height * 0.66 < obj_2_y2:
@@ -63,7 +71,7 @@ def getRelations(img_path):
             # Check relation between Cup and Table:
             elif (obj_1["name"] == TABLE) and (obj_2["name"] not in TABLE_CHAIR):
                 table_height = obj_1_y2 - obj_1_y1
-                if obj_1_x1 < obj_2_x1 + obj_2_width*0.2 and obj_2_x2 - obj_2_width*0.2 < obj_1_x2:
+                if obj_1_x1 < obj_2_x1 + obj_2_width * 0.2 and obj_2_x2 - obj_2_width * 0.2 < obj_1_x2:
                     if (obj_2_y2 < obj_1_y1 + table_height * 0.3) and (obj_1_y1 - table_height * 0.3 < obj_2_y2):
                         print(f'{obj_2["name"]} above table')
                         relations.append(f'{obj_2["name"]} above table')
@@ -73,30 +81,68 @@ def getRelations(img_path):
 
             # Check relation between any objects Left and Right:
             else:
-                if (obj_1_x2 - obj_1_width * 0.5 < obj_2_x1) or (obj_2_x1 + obj_2_width * 0.5 > obj_1_x2):
+                if ((obj_1_x2 - obj_1_width * 0.5 < obj_2_x1) or (
+                        obj_2_x1 + obj_2_width * 0.5 > obj_1_x2)) and obj_1_x1 < obj_2_x1:
                     print(f"{obj_1['name']} left to {obj_2['name']}")
                     relations.append(f"{obj_1['name']} left to {obj_2['name']}")
-                elif (obj_1_x1 + obj_1_width * 0.5 > obj_2_x2) or (obj_2_x2 - obj_2_width * 0.5 < obj_1_x1):
+                elif ((obj_1_x1 + obj_1_width * 0.5 > obj_2_x2) or (
+                        obj_2_x2 - obj_2_width * 0.5 < obj_1_x1)) and obj_1_x2 > obj_2_x2:
                     print(f"{obj_1['name']} right to {obj_2['name']}")
                     relations.append(f"{obj_1['name']} right to {obj_2['name']}")
+                elif ((obj_1["name"] == CHAIR) and (obj_2["name"] == TABLE)) or (
+                        (obj_2["name"] == CHAIR) and (obj_1["name"] == TABLE)):
+                    print(f"CHAIR AND TABLE relations")
+                    relations.append(getRelationByModel(obj_1, obj_2, img_path))
 
     return relations, names
+
+
+def getRelationByModel(obj_1, obj_2, img_path):
+    # Get Coordinates:
+    obj_1_x1, obj_1_y1, obj_1_x2, obj_1_y2 = obj_1["box_points"]
+    obj_2_x1, obj_2_y1, obj_2_x2, obj_2_y2 = obj_2["box_points"]
+
+    img = cv2.imread(img_path)
+    x1 = min(obj_1_x1, obj_2_x1)
+    y1 = min(obj_1_y1, obj_2_y1)
+    x2 = max(obj_1_x2, obj_2_x2)
+    y2 = max(obj_1_y2, obj_2_y2)
+
+    crop_img = img[y1:y2, x1:x2]
+    crop_img = cv2.resize(crop_img, (300, 300))
+    crop_img = np.reshape(crop_img, [1, 300, 300, 3])
+
+    model = load_model('ChairInFrontBehindTable.h5')
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    c = model.predict(crop_img)
+    classes = model.predict_classes(crop_img)
+    classes_proba = model.predict_proba(crop_img)
+
+    print("C:" + str(c))
+    print("Classes:" + str(classes))
+    print(classes_proba)
+
+    if classes[0] == 0:
+        return 'Chair behind table'
+    else:
+        return 'Chair in front of table'
 
 
 if __name__ == "__main__":
     tf.compat.v1.disable_eager_execution()
 
-    # input/output values:
-    input_path = "./input/2.jpg"
-    # relation_array = getRelations(input_path)
-    # print(relation_array)
-
     # Using flask
     app = Flask(__name__)
+
 
     @app.route('/')
     def show_predict_stock_form():
         return render_template('predictorform.html')
+
 
     @app.route('/results', methods=['POST'])
     def upload_file():
@@ -108,6 +154,10 @@ if __name__ == "__main__":
             data = io.BytesIO()
             im.save(data, "JPEG")
             encoded_img_data = base64.b64encode(data.getvalue())
-        return render_template('resultsform.html', relations=relations, names= names, image=encoded_img_data.decode('utf-8'))
+            return render_template('resultsform.html', relations=relations, names=names,
+                                   image=encoded_img_data.decode('utf-8'))
+        else:
+            return '', 204  # Do nothing
+
 
     app.run("localhost", "9999", debug=True)
--- create_model.py
+++ create_model.py
@@ -12,8 +12,7 @@ import numpy as np
 def create_neural_network():
 
     weights_path = "imagenet"
-    weights_path_1 = "C:/Users/roye7/Desktop/Python Projects/Objects Relationship/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5"
-    num_classes = 4
+    num_classes = 2
 
     # optimizer - Adam, binary_crossentropy , multiy label classificatoin
     model = Sequential()
@@ -25,11 +24,11 @@ def create_neural_network():
     return model
 
 
-def fit_model(model_, train_df_, validation_df_):
+def fit_model(model_, train_df_, validation_df_, test_df_):
     BATCH_SIZE = 4
 
     image_gen = ImageDataGenerator(rotation_range=30, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.2,
-                                   zoom_range=0.2, horizontal_flip=False, vertical_flip=False, fill_mode='nearest')
+                                   zoom_range=0.2, horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
 
     # ---- NEW ----
     train_image_gen = image_gen.flow_from_dataframe(
@@ -41,7 +40,7 @@ def fit_model(model_, train_df_, validation_df_):
         class_mode='categorical'
     )
 
-    test_image_gen = image_gen.flow_from_dataframe(
+    validation_image_gen = image_gen.flow_from_dataframe(
         validation_df_,
         x_col='file_name',
         y_col='label',
@@ -50,35 +49,38 @@ def fit_model(model_, train_df_, validation_df_):
         class_mode='categorical'
     )
 
-    """
-    # ---- OLD -----
-    train_image_gen = image_gen.flow_from_directory('C:/Users/roye7/drive_images/Train', target_size=(300, 300),
-                                                    batch_size=BATCH_SIZE, class_mode='categorical')
-
-    test_image_gen = image_gen.flow_from_directory('C:/Users/roye7/drive_images/Validation', target_size=(300, 300),
-                                                   batch_size=BATCH_SIZE, class_mode='categorical')
-    """
+    test_image_gen = image_gen.flow_from_dataframe(
+        test_df_,
+        x_col='file_name',
+        y_col='label',
+        target_size=(300, 300),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical'
+    )
 
     print(train_image_gen.class_indices)
-    print(test_image_gen.class_indices)
+    print(validation_image_gen.class_indices)
 
     # print(model.summary())
-    result = model_.fit(train_image_gen, epochs=100, validation_data=test_image_gen)
+    result = model_.fit(train_image_gen, epochs=100, validation_data=validation_image_gen)
 
-    model_.save("OurModel_NEW.h5")
+    model_.save("ChairInFrontBehindTable.h5")
     print(result)
     print(result.history['accuracy'])
+
+    score = model_.evaluate(test_image_gen)
+    print("Score: " + str(score))
 
 
 def create_df():
     df_array = []
 
     # Go to training folder and read all inside folders
-    inside_folders = ['ChairLeftTable', 'ChairRightTable', 'CupAboveChair', 'CupUnderChair']
+    inside_folders = ['ChairBehindTable', 'ChairFrontTable']
 
     # Create Train dataframe
     for folder in inside_folders:
-        path = f"C:\\Users\\roye7\\drive_images\\Train\\{folder}"
+        path = f"C:\\Users\\roye7\\drive_images_new\\Train\\{folder}"
         files = os.listdir(path)
         df = pd.DataFrame(map(lambda s: path + "\\" + s, files), columns=["file_name"])
         df["label"] = f"[{folder}]"
@@ -89,50 +91,31 @@ def create_df():
 
     # Create Validation dataframe
     for folder in inside_folders:
-        path = f"C:\\Users\\roye7\\drive_images\\Validation\\{folder}"
+        path = f"C:\\Users\\roye7\\drive_images_new\\Validation\\{folder}"
         files = os.listdir(path)
         df = pd.DataFrame(map(lambda s: path + "\\" + s, files), columns=["file_name"])
         df["label"] = f"[{folder}]"
         df_array.append(df)
     validation_df_ = pd.concat(df_array)
+    df_array.clear()
 
-    return train_df_, validation_df_
+    # Create Test dataframe
+    for folder in inside_folders:
+        path = f"C:\\Users\\roye7\\drive_images_new\\Test\\{folder}"
+        files = os.listdir(path)
+        df = pd.DataFrame(map(lambda s: path + "\\" + s, files), columns=["file_name"])
+        df["label"] = f"[{folder}]"
+        df_array.append(df)
+    test_df_ = pd.concat(df_array)
+
+    return train_df_, validation_df_, test_df_
 
 
 if __name__ == "__main__":
-    """
+
     # Create Model:
-    train_df, validation_df = create_df()
+    train_df, validation_df, test_df = create_df()
 
     model = create_neural_network()
-    fit_model(model, train_df, validation_df)
-    """
+    fit_model(model, train_df, validation_df, test_df)
 
-    image_gen = ImageDataGenerator(rotation_range=30, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.2,
-                                   zoom_range=0.2, horizontal_flip=False, vertical_flip=False, fill_mode='nearest')
-    train_image_gen = image_gen.flow_from_directory('C:/Users/roye7/drive_images/Train', target_size=(300, 300),
-                                                    batch_size=4, class_mode='categorical')
-    print(train_image_gen.class_indices)
-
-    # Use exist model:
-    model = load_model('OurModel_NEW.h5')
-
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-
-    img = cv2.imread('input/6.jpeg')
-    img = cv2.resize(img, (300, 300))
-    img = np.reshape(img, [1, 300, 300, 3])
-
-    c = model.predict(img)
-    classes = model.predict_classes(img)
-    classes_proba = model.predict_proba(img)
-
-    # get predicted class name and print:
-    position = list(train_image_gen.class_indices.values()).index(classes[0])
-    print(list(train_image_gen.class_indices.keys())[position])
-
-    print(c)
-    print(classes)
-    print(classes_proba)
